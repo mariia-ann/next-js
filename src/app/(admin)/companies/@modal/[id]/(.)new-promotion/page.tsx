14777202a4a96ed625624d48125930e4aac9ed9a
Mariia <myshyck@gmail.com>
2025-09-12 12:21:27 +0300

    add eslint rules

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,11 +1,14 @@
 'use client';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { useRouter } from 'next/navigation';
 import PromotionFormModal from '@/app/components/promotion-form-modal';
 
+// @ts-ignore – Next.js bug with intercepting routes
+
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
-  
+
   return (
     <PromotionFormModal
       companyId={params.id}
